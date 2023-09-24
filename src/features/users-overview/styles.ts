4cf84da68737258f311d48a4edc46909e2b583f5
hasanqzuyer <work@teleint.com>
2023-09-24 16:32:45 +0200

    Fixed responsive design on the users-overview page for tablets and mobile devices.

--- src/features/users-overview/styles.ts
+++ src/features/users-overview/styles.ts
@@ -23,11 +23,24 @@ export const AccountGrid = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 16px 177px;
   padding-right: 90px;
+  @media screen and (max-width: 768px) {
+    gap: 20px 50px;
+    padding-right: 20px;
+  }
+  @media screen and (max-width: 425px) {
+    display: flex;
+    flex-direction: column;
+    gap: 25px 50px;
+    padding-right: 10px;
+  }
 `;
 
 export const PreferenceEditButton = styled.div`
   cursor: pointer;
   padding-left: 20px;
+  @media screen and (max-width: 768px) {
+    padding-left: 10px;
+  }
 `;
 
 export const SkillGrid = styled.div`
