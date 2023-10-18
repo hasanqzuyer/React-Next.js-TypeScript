e0c8a1f7218534c2fd718934a2f3b2353672a8c0
hasanqzuyer <work@teleint.com>
2023-10-18 22:51:28 +0200

    a

--- src/components/core/title/index.tsx
+++ src/components/core/title/index.tsx
@@ -8,6 +8,7 @@ const { prefix, sufix } = Project.title;
 const Title = ({ children }: TitleType) => (
   <Head>
     <title>{[prefix, String(children), sufix].join('')}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
   </Head>
 );
 
